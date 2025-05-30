02e1a5ca0f97441cbf0dae068e8cab232b16f7f0
Dev-Alexandre-Ferreira <Dev.alexandre1@outlook.comgit config --global user.name Dev-Alexandre-Ferreira>
2025-05-30 13:01:09 -0300

    Continuidade das atividades

--- Exercicio pg 26 3.10/exercicioC.js
+++ Exercicio pg 26 3.10/exercicioC.js
@@ -9,22 +9,22 @@ const votosNulos = parseInt(prompt("Digite a quantidade de votos nulos"));
 const totalEleitores = votosA + votosB + votosC + votosBranco + votosNulos;
 
 function candidatoA(votosA, totalEleitores) {
-  return (votosA / 100) * totalEleitores;
+  return (votosA * 100) / totalEleitores;
 }
 function candidatoB(votosB, totalEleitores) {
-  return (votosB / 100) * totalEleitores;
+  return (votosB * 100) / totalEleitores;
 }
 function candidatoC(votosC, totalEleitores) {
-  return (votosC / 100) * totalEleitores;
+  return (votosC * 100) / totalEleitores;
 }
 function totalBranco(votosBranco, totalEleitores) {
-  return (votosBranco / 100) * totalEleitores;
+  return (votosBranco * 100) / totalEleitores;
 }
 function totalNulos(votosNulos, totalEleitores) {
-  return (votosNulos / 100) * totalEleitores;
+  return (votosNulos * 100) / totalEleitores;
 }
 
-const votosValidos = votosA + votosB + votosC;
+const votosValidos = ((votosA + votosB + votosC) * 100) / totalEleitores;
 
 alert(`Número total de Eleitores: ${totalEleitores}
   Porcentagem de votos Válidos: ${votosValidos}%
